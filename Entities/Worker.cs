b4b0257b38d287606105676092df6b22d2ef2f5d
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-06 15:39:50 -0300

    Project Worker Salary

---
+++ Entities/Worker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using WorkerSalary.Entities.Enums;
+using System.Collections.Generic;
+
+namespace WorkerSalary.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+
+        public Worker() { 
+        }
+
+        public Worker(string name, WorkerLevel level, double basesalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = basesalary;
+            Departament = departament;
+        }
+
+
+
+
+    }
+
+}
