01f56cb0cfa2dbecefccff95b999c67bb03cb3e7
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-06 15:50:10 -0300

    Creating classes and methods

--- Entities/Worker.cs
+++ Entities/Worker.cs
@@ -25,6 +25,30 @@ namespace WorkerSalary.Entities
             Departament = departament;
         }
 
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year,  int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+
+
 
 
 
